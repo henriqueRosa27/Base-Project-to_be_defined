a276ad68f07845544c00bec28d1a16e04ab9c51a
pc_home <rosa.henrique@outlook.com.br>
2020-07-13 13:32:27 -0300

    resolve problems on validate email

--- src/application/validations/user.validations.ts
+++ src/application/validations/user.validations.ts
@@ -1,9 +1,4 @@
 import * as Yup from 'yup';
-import { IUserService } from 'src/services/iservices';
-import { UserService } from 'src/services/services/user.service';
-import { UserRepository } from 'src/infrastructure/repositories';
-import { Repository } from 'typeorm';
-import { UserEntity } from 'src/domain/entities';
 
 export const createUserValidation = Yup.object({
   name: Yup.string()
@@ -17,15 +12,7 @@ export const createUserValidation = Yup.object({
   email: Yup.string()
     .required('Campo obrigatório')
     .email('E-mail inválido')
-    .max(100)
-    .test('unique', 'Email já cadastrado', async value => {
-       const userService: IUserService = new UserService(
-         new UserRepository(new Repository<UserEntity>()),
-       );
-       const t = await userService.get().catch(e => console.log(e));
-       console.log(t)
-      return false;
-    }),
+    .max(100),
   password: Yup.string()
     .required('Campo obrigatório')
     .min(8, 'Minimo de 8 caracteres')
